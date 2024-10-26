528076ec076d50a9b381041bf1f03e4e97d853ff
Konstantin-1x <kostia.haperskiy@mail.ru>
2024-10-26 20:12:29 +0300

    1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,10 @@
 package org.example;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Main {
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "E:\\ProjectJava\\parser-flot\\chromedriver-win64\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "chromedriver-win64\\chromedriver.exe");
         Parser parser = new Parser();
-        parser.Course1();
+        parser.Course1("https://doninturflot.com/catalog/date-from-01.01.2025-to-31.12.2025/ship-shevthenko_aleksandra/");
     }
 }
--- src/main/java/org/example/Parser.java
+++ src/main/java/org/example/Parser.java
@@ -5,15 +5,32 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.By;
+import java.io.FileWriter;
+import java.io.IOException;
+
+
 
 public class Parser {
-    public void Course1(){
-        System.setProperty("webdriver.chrome.driver", "E:\\ProjectJava\\parser-flot\\chromedriver-win64\\chromedriver.exe");
+    public void Course1(String args){
+        System.out.println("ну чтож, начнем");
         WebDriver webDriver = new ChromeDriver();
-        webDriver.get("https://doninturflot.com/catalog/date-from-01.01.2025-to-31.12.2025/ship-shevthenko_aleksandra/");
-        System.out.println(webDriver.getTitle());
 
+        webDriver.get(args);
+        List<WebElement> elements = webDriver.findElements(By.xpath("//*"));
+        StringBuilder allText = new StringBuilder();
+        for (WebElement element : elements) {
+            allText.append(element.getText()).append("\n");
+        }
 
+        // Записываем текст в файл
+        try (FileWriter writer = new FileWriter("text.txt")) {
+            writer.write(allText.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Закрываем браузер
         webDriver.quit();
+
     }
 }
