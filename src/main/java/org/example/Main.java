022f1dcefd86a1b71ec23a58486c67e5aec772bc
Konstantin-1x <kostia.haperskiy@mail.ru>
2024-10-26 19:03:01 +0300

    1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,12 @@
 package org.example;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.setProperty("webdriver.chrome.driver", "E:\\ProjectJava\\parser-flot\\chromedriver-win64\\chromedriver.exe");
+        Parser parser = new Parser();
+        parser.Course1();
     }
 }
--- src/main/java/org/example/Parser.java
+++ src/main/java/org/example/Parser.java
@@ -1,4 +1,19 @@
 package org.example;
 
+import java.util.List;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.By;
+
 public class Parser {
+    public void Course1(){
+        System.setProperty("webdriver.chrome.driver", "E:\\ProjectJava\\parser-flot\\chromedriver-win64\\chromedriver.exe");
+        WebDriver webDriver = new ChromeDriver();
+        webDriver.get("https://doninturflot.com/catalog/date-from-01.01.2025-to-31.12.2025/ship-shevthenko_aleksandra/");
+        System.out.println(webDriver.getTitle());
+
+
+        webDriver.quit();
+    }
 }
