13067fd856bbe155b6f3427e8f796abd11c33318
xRESTx <miha181104@mail.ru>
2024-10-26 21:59:34 +0300

    work+-

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Main {
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\REST\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+        System.setProperty("webdriver.firefox.driver", "C:\\Users\\REST\\Downloads\\geckodriver-v0.35.0-win64\\geckodriver.exe");
         Parser parser = new Parser();
         parser.Course1("https://doninturflot.com/catalog/date-from-01.01.2025-to-31.12.2025/ship-shevthenko_aleksandra/");
     }
