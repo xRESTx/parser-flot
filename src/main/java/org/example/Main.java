66e3dfeacb43d1352e7c68cbe21e479b0310529f
Konstantin-1x <kostia.haperskiy@mail.ru>
2024-10-27 23:01:48 +0300

    Parser_gama

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,13 +7,20 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         System.setProperty("webdriver.chrome.driver", "geckodriver-v0.35.0-win64\\geckodriver.exe");
-        Parser parser = new Parser();
-        FileWriter fileWriter = new FileWriter("cruises.txt");
-        //запуск одного resulta
-        //parser.Course1("https://doninturflot.com/catalog/cruises-from-rostov_on_don/to-rostov_on_don/date-from-01.01.2025-to-31.12.2025/ship-shevthenko_aleksandra/");
-        //прогон всех рейсов 2025 года
-        for (int numberPage = 1; numberPage<23; numberPage++){
-            parser.Course1("https://doninturflot.com/catalog/date-from-01.01.2025-to-31.12.2025/?PAGEN_1="+numberPage);
-        }
+        Parser_gama parserGama = new Parser_gama();
+        parserGama.Course("https://gama-nn.ru/cruise/aurum/");
+
+
+
+
+        //parsing with https://doninturflot.com/catalog/cruises-from-rostov_on_don/to-rostov_on_don/
+//        Parser parser = new Parser();
+//        FileWriter fileWriter = new FileWriter("cruises.txt");
+//        //запуск одного resulta
+//        //parser.Course1("https://doninturflot.com/catalog/cruises-from-rostov_on_don/to-rostov_on_don/date-from-01.01.2025-to-31.12.2025/ship-shevthenko_aleksandra/");
+//        //прогон всех рейсов 2025 года
+//        for (int numberPage = 1; numberPage<23; numberPage++){
+//            parser.Course1("https://doninturflot.com/catalog/date-from-01.01.2025-to-31.12.2025/?PAGEN_1="+numberPage);
+//        }
     }
 }
