f4691ec1081764897dadc2724fe3e234939dc7ee
Konstantin-1x <kostia.haperskiy@mail.ru>
2024-11-05 23:10:49 +0300

    Update parse and create format

--- src/main/java/org/example/Parser_Vodohod.java
+++ src/main/java/org/example/Parser_Vodohod.java
@@ -9,19 +9,24 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
 public class Parser_Vodohod {
     public void Course(String url) {
         WebDriver webDriver = new FirefoxDriver();
-
+        Format format = new Format();
         try (BufferedWriter writer = new BufferedWriter(new FileWriter("vodohod.txt",true))) {
             webDriver.get(url);
-            Thread.sleep(2000);
             System.out.println("Okay, let's go");
             List<WebElement> elemets = webDriver.findElements(By.cssSelector(".p-content__inner__wrapper a"));
             for(WebElement elemet : elemets){
+                ArrayList<String> city = new ArrayList<>();
+                ArrayList<String> timeIn = new ArrayList<>();
+                ArrayList<String> timeOut = new ArrayList<>();
+                ArrayList<String> timeDay = new ArrayList<>();
+
                 String strhref  = elemet.getAttribute("href");
                 ((JavascriptExecutor) webDriver).executeScript("window.open('" + strhref + "', '_blank');");
                 String originalTab = webDriver.getWindowHandle();
@@ -32,12 +37,50 @@ public class Parser_Vodohod {
                         break;
                     }
                 }
-                Thread.sleep(6000);
+                Thread.sleep(5000);
                 WebElement button = webDriver.findElement(By.className("b-rc__view-all-btn"));
+                ((JavascriptExecutor) webDriver).executeScript("arguments[0].scrollIntoView({block: 'center', behavior: 'smooth'});", button);
                 ((JavascriptExecutor)webDriver).executeScript("arguments[0].click();", button);
-                Thread.sleep(3000);
+                Thread.sleep(1000);
+                WebElement teplohod = webDriver.findElement(By.cssSelector(".booking__left-item.booking__left-ship"));
+                WebElement pteplohod = teplohod.findElement(By.cssSelector(".booking__i-value"));
+                String nameTeplohod = pteplohod.getText();
+//                System.out.println(nameTeplohod);
 
-
+                List<WebElement> days = webDriver.findElements(By.cssSelector(".b-spoiler__content"));
+                for (WebElement day : days){
+                    List<WebElement> bdays = day.findElements(By.cssSelector(".b-day"));
+                    int condition = 0;
+                    for(WebElement bday : bdays){
+                        city.add("");
+                        timeIn.add("");
+                        timeOut.add("");
+                        timeDay.add("");
+                        WebElement acity = bday.findElement(By.cssSelector(".body-content__text-title-main.link"));
+                        city.add(acity.getText());
+                        List<WebElement> dates = bday.findElements(By.cssSelector(".b-day__grid-item"));
+                        for(WebElement date : dates){
+                            String temp1 = date.getText();
+                            String temp2 = temp1.split("\n")[0];
+                            String temp3 = temp1.split("\n")[1];
+                            if (temp2.length() == 5) {
+                                temp3 = temp3.split(" ")[0];
+                                timeDay.set(condition,temp3);
+                                System.out.println("Дата: " + timeDay.get(condition));
+                            }
+                            if (temp2.length() == 9) {
+                                timeIn.set(condition,temp3);
+                                System.out.println("Прибытие: " + timeIn);
+                            }
+                            if (temp2.length() == 12) {
+                                System.out.println("Отправление: " + temp3);
+                                timeOut.set(condition,temp3);
+                            }
+                        }
+                        condition++;
+                    }
+                }
+                format.FormatVodohodInturStopFromTXT(nameTeplohod, strhref,timeDay,city,timeIn,timeOut,writer);
                 webDriver.close();
                 webDriver.switchTo().window(originalTab);
             }
@@ -46,6 +89,8 @@ public class Parser_Vodohod {
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }finally {
+            webDriver.manage().deleteAllCookies();
+            webDriver.quit();
             System.out.println("Strange huinya, My Lord");
         }
     }
